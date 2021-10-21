4b921b9aaec7f4f3bad77f76e84214ab1a90ebbc
umardeveloper <umar.abduganiyev@yandex.ru>
2021-10-21 07:07:32 -0700

    add corusel

--- js/main.js
+++ js/main.js
@@ -72,5 +72,10 @@ elNextButton.addEventListener("click", () => {
   page++;
   getMovies();
 });
-
+simpleslider.getSlider({
+  container: document.getElementById("myslider"),
+  transitionTime: 1,
+  delay: 3.5,
+  prop: "left",
+});
 getMovies();
